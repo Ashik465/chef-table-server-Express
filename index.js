d2f75b4d3c9aa15bdfa40fd55f214906306f932a
Ashik465 <helloashikul@gmail.com>
2023-05-03 07:09:20 +0600

    chef-data-running-on-server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const  cors = require('cors');
 const app = express()
+app.use(cors());
 const port = 5000
+
+const chefData = require('./data.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/chefdata', (req, res) => {
+  res.send(chefData)
 })
 
 app.listen(port, () => {
